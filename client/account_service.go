499ed1d15cd711b71eeb6845ce82555f9ada9d59
Lucas Noblat <lucas.almeida@al.infnet.edu.br>
2021-08-10 23:07:21 -0300

    changes account service
    adds some improving

--- client/account_service.go
+++ client/account_service.go
@@ -8,50 +8,56 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
 	"github.com/google/uuid"
 )
 
-func buildAndMakeAccountRequest(body *account.RequestData, method string, url string) *http.Response {
-
-	client := &http.Client{}
+func buildAndMakeAccountRequest(body *account.RequestData, method string, url string) (*http.Response, error) {
 	baseUrl := os.Getenv("BASE_URL")
 	httpdeleteurl := baseUrl + url
 
 	if body != nil {
-		requestString, _ := json.Marshal(body)
-		byteBody := bytes.NewBuffer(requestString)
-		req, _ := http.NewRequest(method, httpdeleteurl, byteBody)
-		resp, err := client.Do(req)
+		jsonBytes, _ := json.Marshal(body)
+		request, _ := http.NewRequest(method, httpdeleteurl, bytes.NewBuffer(jsonBytes))
+		request.Header.Add("Content-Type", "application/json")
+		response, err := makeRequest(request)
+		return response, err
 	} else {
-		req, err := http.NewRequest(method, httpdeleteurl, nil)
+		request, _ := http.NewRequest(method, httpdeleteurl, nil)
+		response, err := makeRequest(request)
+		return response, err
 	}
-	resp, err := client.Do(req)
+}
+
+func makeRequest(request *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
+	}
+	defer response.Body.Close()
+	return response, err
+}
+
+func Create(acc *account.Attributes) (string, error) {
+
+	accData := &account.AccountInfo{
+		Attributes:     acc,
+		ID:             uuid.New().String(),
+		Type:           "accounts",
+		OrganisationID: uuid.New().String(),
 	}
 
-	defer resp.Body.Close()
-
-	return resp
-}
-
-func configureHttpClient() {
-	client := Client{}
-	c := config.Schema{}
-}
-
-func Create(acc *account.RequestData) (string, error) {
-
-	info := account.AccountInfo{
-		Attributes:     accAttributes,
-		ID:             uuid.New(),
-		Type:           "accounts",
-		OrganisationID: uuid.New(),
+	requestData := &account.RequestData{
+		RequestBody: accData,
 	}
 
 	url := "v1/organisation/accounts"
-	response := buildAndMakeAccountRequest(acc, "POST", url)
+	response, err := buildAndMakeAccountRequest(requestData, "POST", url)
+
+	if err != nil {
+		return "", err
+	}
 
 	if response.StatusCode == 409 {
 		return "", fmt.Errorf("account already exists")
@@ -59,37 +65,47 @@ func Create(acc *account.RequestData) (string, error) {
 
 	logResponse(response)
 
-	return string(body), error
+	body, _ := ioutil.ReadAll(response.Body)
+	return string(body), nil
 }
 
 func FetchById(id string) (string, error) {
 
 	url := "v1/organisation/accounts" + "/" + id
-	response := buildAndMakeAccountRequest(nil, "GET", url)
+	response, err := buildAndMakeAccountRequest(nil, "GET", url)
+
+	if err != nil {
+		return "", err
+	}
 
 	if response.StatusCode == 404 {
 		return "", fmt.Errorf("account not found")
 	}
 
 	logResponse(response)
+	body, _ := ioutil.ReadAll(response.Body)
 	return string(body), nil
 }
 
 func Delete(id string) (string, error) {
 
 	url := "v1/organisation/accounts" + "/" + id
-	response := buildAndMakeAccountRequest(nil, "DELETE", url)
+	response, err := buildAndMakeAccountRequest(nil, "DELETE", url)
+
+	if err != nil {
+		return "", err
+	}
 
 	if response.StatusCode == 404 {
 		return "", fmt.Errorf("account not found")
 	}
 
 	logResponse(response)
-
+	body, _ := ioutil.ReadAll(response.Body)
 	return string(body), nil
 }
 
-func logResponse(resp *http.Response) {
+func logResponse(response *http.Response) {
 	fmt.Println("response Status:", response.Status)
 	fmt.Println("response Headers:", response.Header)
 	body, _ := ioutil.ReadAll(response.Body)
--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	clientservice "form3/client"
-	account "form3/model"
-)
-
 func main() {
-	country := "GB"
-	accAttributes := &account.Attributes{
-		Country: &country,
-		Name:    []string{"United Kingdom", "UK"},
-	}
-	accData := &account.AccountInfo{
-		Attributes:     accAttributes,
-		ID:             "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc",
-		Type:           "accounts",
-		OrganisationID: "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
-	}
-	requestData := &account.RequestData{
-		RequestBody: accData,
-	}
 
-	clientservice.NewAccount(requestData)
 }
